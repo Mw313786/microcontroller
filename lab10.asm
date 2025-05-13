2589f7e97064a3151a8c0aac446f584863d3bc4d
Mw313786 <mohammed.23cs074@sode-edu.in>
2025-05-13 15:42:26 +0530

    Add files via upload

---
+++ lab10.asm
@@ -0,0 +1,12 @@
+	AREA DEMO,CODE,READONLY
+START
+;disabling
+	mrs r0,cpsr;11010011=D3
+	BIC r0,r0,#0x80;01010011=53
+	msr cpsr_c,r0;
+;enabling
+	mrs r0,cpsr;
+	ORR r0,r0,#0x80
+	msr cpsr_c,r0;
+	BX LR
+	END
